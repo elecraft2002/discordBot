ee9bc34e5c1ee037a3153b9cb63790bee2763e1d
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2021-12-09 20:18:11 +0100

    main

--- public/memeCreator/meme.js
+++ public/memeCreator/meme.js
@@ -6,25 +6,6 @@ require('dotenv').config()
 
 async function createMeme(url, top, bottom, width, height) {
     console.log("rendering")
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Meme xd</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <img src="${url}">
-        <p class="top">${top}</p>
-        <p class="bottom">${bottom}</p>
-    </body>
-    </html>`
-    fs.writeFile("public/memeCreator/index.html", html, err => {
-        if (err)
-            console.log(err)
-    })
     console.log(process.env.URL_PATH + "/memeCreator/index.html")
     const browser = await puppeteer.launch({
         args: [
@@ -36,6 +17,12 @@ async function createMeme(url, top, bottom, width, height) {
     await page.setViewport({ width, height })
     await page.goto(process.env.URL_PATH + "/memeCreator/index.html");
     const path = 'public/imgs/meme.png'
+    await page.evaluate((url, top, bottom) => {
+        document.getElementById("img").src = url
+        document.getElementById("top").innerText = top
+        document.getElementById("bottom").innerText = bottom
+    }, url, top, bottom)
+    await page.waitForSelector("#img")
     await page.screenshot({ path });
     await browser.close();
     return path
